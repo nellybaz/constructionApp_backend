baecafdea5fac8092b1f4d60131b151d74c45598
Nelson Bassey <nellybaz10@gmail.com>
2019-07-01 15:35:56 +0200

    setup app on heroku

--- constructionapp/settings.py
+++ constructionapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,3 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
